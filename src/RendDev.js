2af70dc7107d96721ab1613f668ebf569367a6e3
AlvvxL <44138315+AlvvxL@users.noreply.github.com>
2019-03-29 22:40:12 +0700

    updated

--- src/RendDev.js
+++ src/RendDev.js
@@ -1,4 +1,5 @@
 const request = require("node-superfetch");
+const fetchUser = require("./fetchUser");
 
 module.exports = class RendDev {
     constructor(botID, ownerID) {
@@ -8,18 +9,20 @@ module.exports = class RendDev {
         if (typeof ownerID !== "string") throw new Error("Owner ID must be a string")
         if (isNaN(botID)) throw new Error("Invalid Bot ID.");
         if (isNaN(ownerID)) throw new Error("Invalid Owner ID.");
-        // if (botID.length > 18) throw new Error("Invalid Bot Id.");
-        // if (ownerID.length > 18) throw new Error("Invalid Owner Id.");
-        fetchUser(ownerID).then(owner => {
-        fetchUser(botID).then(botOwn => {
-        console.log(`You logged as ${owner.tag} with bot ${botOwn.tag}`);
+        if (botID.length < 17) throw new Error("Invalid Bot Id.");
+        if (ownerID.length < 17) throw new Error("Invalid Owner Id.");
+        
+        request.get(`https://rend-dev.glitch.me/api/bots/${botID}`).then(rend => {
+            if (ownerID !== rend.body.ownerID) throw new Error("Wrong Owner ID.")
+            else {
+                fetchUser(ownerID).then(owner => {
+                    fetchUser(botID).then(botOwn => {
+                        console.log(`You logged as ${owner.tag} with bot ${botOwn.tag}`);
+                    })
+                })
+            }
         })
-        })
-        /*
-        fetch.get(this.baseAPIURL + `/bots/${botID}`).then(rend => {
-            if (ownerID !== rend.body.ownerID) throw new Error("Wrong Owner ID.");
-        })
-        */
+        
         this.baseURL = "https://rend-dev.glitch.me";
         this.baseAPIURL = this.baseURL + "/api";
         
@@ -46,45 +49,18 @@ module.exports = class RendDev {
             const owner = await fetchUser(botuser.ownerID);
             const bot = await fetchUser(botuser.botID);
             const body = {
-                owner: {
-                    id: owner.id,
-                    username: owner.username,
-                    tag: owner.tag,
-                    avatar: owner.avatar,
-                    avatarURL: owner.avatarURL,
-                    displayAvatarURL: owner.displayAvatarURL,
-                    bot: owner.bot,
-                    createdTimestamp: owner.createdTimestamp,
-                    createdAt: new Date(owner.createdTimestamp),
-                    bots : owner.bots
-                },
-                bot: {
-                    id: bot.id,
-                    username: bot.username,
-                    tag: bot.tag,
-                    avatar: bot.avatar,
-                    avatarURL: bot.avatarURL,
-                    displayAvatarURL: bot.displayAvatarURL,
-                    bot: bot.bot,
-                    createdTimestamp: bot.createdTimestamp,
-                    createdAt: new Date(bot.createdTimestamp),
-                },
+                owner,
+                bot,
                 prefix: botuser.prefix,
                 accepted: botuser.accepted
             };
-        return body;
+            return body;
         };
 
         this.fetchUser = async (id) => {
             if (!id) throw Error("No bot ID was provided");
             const user = await fetchUser(id);
-        return user;
+            return user;
         };
     };
-};
-
-const fetchUser = async (userID) => {
-    const { body } = await request.get(`https://rend-dev.glitch.me/api/fetchUser?id=${userID}`);
-    if (body.error === "invalid_id") throw new Error("Invalid User ID");
-    return body;
 };
---
+++ src/fetchUser.js
@@ -0,0 +1,42 @@
+const request = require("node-superfetch");
+
+module.exports = async (userID) => {
+    const { body } = await request.get(`https://rend-dev.glitch.me/api/fetchUser?id=${userID}`);
+    if (body.error === "invalid_id") throw new Error("Invalid User ID");
+    else {
+        if (body.bot === true) {
+            const { body: bot } = (await request.get(`https://rend-dev.glitch.me/api/bots/${userID}`))
+            const { body: fetchedOwnerID } = await request.get(`https://rend-dev.glitch.me/api/fetchUser?id=${bot.ownerID}`);
+            fetchedOwnerID.createdAt = new Date(fetchedOwnerID.createdTimestamp).toString();
+            let user = {
+                id: body.id,
+                username: body.username,
+                discriminator: body.discriminator,
+                tag: body.tag,
+                avatar: body.avatar,
+                avatarURL: body.avatarURL,
+                displayAvatarURL: body.displayAvatarURL,
+                bot: body.bot,
+                createdTimestamp: body.createdTimestamp,
+                createdAt: new Date(body.createdTimestamp).toString(),
+                owner: fetchedOwnerID
+            };
+            return user;
+        } else {
+            let user = {
+                id: body.id,
+                username: body.username,
+                discriminator: body.discriminator,
+                tag: body.tag,
+                avatar: body.avatar,
+                avatarURL: body.avatarURL,
+                displayAvatarURL: body.displayAvatarURL,
+                bot: body.bot,
+                createdTimestamp: body.createdTimestamp,
+                createdAt: new Date(body.createdTimestamp).toString(),
+                bots: body.bots
+            };
+            return user;
+        };
+    };
+};
